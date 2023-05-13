1e994c39a0359b36335f9b193370711ce3e4d0fc
Gabriel <gabrielvasconcelosv21@gmail.com>
2023-05-13 13:43:10 -0300

    ListaDecicao

---
+++ listadeexerciciosdecisao/E2.java
@@ -0,0 +1,8 @@
+package listadeexerciciosdecisao;
+import java.util.Scanner;
+public class E2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
